04aebe0f4da00381f4a70637d7e94862b95154b6
bbaoomj <96920917+bbaoomj@users.noreply.github.com>
2022-01-01 20:11:28 +0300

    Update and rename plugins/callback.py to الإضافات/callback.py

--- الإضافات/callback.py
+++ الإضافات/callback.py
@@ -224,8 +224,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/o604254'),
+                InlineKeyboardButton('🧩 Source', url='https://t.me/o604254'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
