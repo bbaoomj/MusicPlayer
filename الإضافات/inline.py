57352e56988f7258d04032b6ab12cfb36519b71f
bbaoomj <96920917+bbaoomj@users.noreply.github.com>
2022-01-01 19:45:53 +0300

    Update and rename plugins/inline.py to الإضافات/inline.py

--- الإضافات/inline.py
+++ الإضافات/inline.py
@@ -28,8 +28,8 @@ from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('⚡️Make Own Bot', url=''),
+        InlineKeyboardButton('🧩 Source Code', url=''),
     ],
     [
         InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
